655c300cbbdcb8630bd96699ca23b634d8cdc39b
杨智文 <yangzhiwen@chalco-steering.com>
2019-04-30 16:51:28 +0800

    跨域处理

--- src/main/java/com/boot/EedaServiceApplication.java
+++ src/main/java/com/boot/EedaServiceApplication.java
@@ -2,11 +2,13 @@ package com.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@ServletComponentScan("com.boot.common.filter")
 public class EedaServiceApplication {
 
 	public static void main(String[] args) {
